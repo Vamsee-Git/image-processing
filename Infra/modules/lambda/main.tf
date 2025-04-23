2c801c117f0ca51dd3243c213132adc72d7cc4de
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-23 11:40:49 +0530

    Create main.tf

---
+++ Infra/modules/lambda/main.tf
@@ -0,0 +1,23 @@
+resource "aws_lambda_function" "image_processor" {
+  function_name = var.function_name
+  role          = var.role_arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+  filename      = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      DEST_BUCKET = var.destination_bucket
+    }
+  }
+}
+
+resource "aws_s3_bucket_notification" "source_bucket_notification" {
+  bucket = var.source_bucket
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.image_processor.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
