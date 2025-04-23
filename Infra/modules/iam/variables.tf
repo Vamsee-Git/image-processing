ac7b2bbc7e708e76b67560ad1550a42a9020d509
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-23 11:46:12 +0530

    Create variables.tf

---
+++ Infra/modules/iam/variables.tf
@@ -0,0 +1,14 @@
+variable "role_name" {
+  description = "The name of the IAM role"
+  type        = string
+}
+
+variable "source_bucket_name" {
+  description = "The name of the source S3 bucket"
+  type        = string
+}
+
+variable "destination_bucket_name" {
+  description = "The name of the destination S3 bucket"
+  type        = string
+}
