94664a79d7ac5d9e8b15806bfc3d5fa46115b5d4
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-23 11:41:43 +0530

    Create variables.tf

---
+++ Infra/modules/lambda/variables.tf
@@ -0,0 +1,19 @@
+variable "function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+}
+
+variable "role_arn" {
+  description = "The ARN of the IAM role for Lambda"
+  type        = string
+}
+
+variable "source_bucket" {
+  description = "The name of the source S3 bucket"
+  type        = string
+}
+
+variable "destination_bucket" {
+  description = "The name of the destination S3 bucket"
+  type        = string
+}
