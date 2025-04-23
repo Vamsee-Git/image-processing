037dd5edb6ffbe3d5d2e99b4385cdb590ff1974f
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-23 16:10:43 +0530

    Update outputs.tf

--- Infra/modules/s3/outputs.tf
+++ Infra/modules/s3/outputs.tf
@@ -2,7 +2,12 @@ output "bucket_name" {
   value = aws_s3_bucket.bucket.bucket
 }
 
-output "bucket_name" {
+output "bucket_arn" {
   value = aws_s3_bucket.bucket.arn
 }
+
+output "bucket_id" {
+  value = aws_s3_bucket.bucket.id
+}
+
 
