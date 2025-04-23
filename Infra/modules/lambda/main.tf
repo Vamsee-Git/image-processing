3e53d9d29323cbd425d7cb956e58f0086083445e
Vamsee-Git <95101547+Vamsee-Git@users.noreply.github.com>
2025-04-23 16:02:05 +0530

    Update main.tf

--- Infra/modules/lambda/main.tf
+++ Infra/modules/lambda/main.tf
@@ -13,11 +13,22 @@ resource "aws_lambda_function" "image_processor" {
   }
 }
 
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.image_processor.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.source_bucket.arn
+}
+
 resource "aws_s3_bucket_notification" "source_bucket_notification" {
-  bucket = var.source_bucket
+  bucket = aws_s3_bucket.source_bucket.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.image_processor.arn
     events              = ["s3:ObjectCreated:*"]
   }
+
+  depends_on = [aws_lambda_permission.allow_s3_invoke]
 }
+
